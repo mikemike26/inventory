71c076cc91725d5bf4b7cd6c4e5f7fb5c5aabe46
Mike Rensel <mike.rensel@mktec.com>
2015-01-18 17:35:03 -0500

    nav styles

--- js/routes.js
+++ js/routes.js
@@ -1,27 +1,15 @@
-salesTool.config([ '$urlRouterProvider', '$stateProvider',
+ia.config([ '$urlRouterProvider', '$stateProvider',
 		function($urlRouterProvider, $stateProvider) {
-			$urlRouterProvider.otherwise('/main');
-
+			$urlRouterProvider.otherwise('/dashboard/main');
 			$stateProvider
-			.state("login", {
-				url : '/login',
-				templateUrl : 'templates/pages/login.html'
-			})
             .state("dashboard", {
                 url : '/dashboard',
-                templateUrl : 'templates/layout/store-dashboard.html',
-                abstract: true,
-                controller: function($scope, $state) {
-                    $scope.$state = $state;
-                }
+                templateUrl : 'templates/layout/dashboard.html',
+                abstract: true
             })
 			.state("dashboard.main", {
 				url : '/main',
-                views : {
-                    "header" : {templateUrl: "templates/layout/dashboard-header.html"},
-                    "content" : {templateUrl: "templates/pages/main.html"},
-                    "nav" : {templateUrl: "templates/layout/dashboard-nav.html"}
-                 }
+                templateUrl : 'templates/pages/main.html'
 			})
 
 } ]);
